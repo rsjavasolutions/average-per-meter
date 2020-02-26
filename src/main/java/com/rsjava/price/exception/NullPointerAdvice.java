b842e162f1a1315554225844346b4e8d48342bd6
rsjavasolutions <stan.robert.1986@gmail.com>
2020-02-26 22:56:20 +0100

    New class NullPointer Advice

---
+++ src/main/java/com/rsjava/price/exception/NullPointerAdvice.java
@@ -0,0 +1,21 @@
+package com.rsjava.price.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class NullPointerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+
+    public String nullPointerHandler(NullPointerException ex){
+        return "There is a connection problem, change the data and try again.";
+    }
+
+
+}
